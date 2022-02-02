4c3fda154f08b04edff17387aa8789cc15ae2daa
Ubuntu <ubuntu@vm1.us-west4-b.c.helical-ranger-337914.internal>
2022-02-02 18:12:55 +0000

    Add cryptomateriels for all organizations

--- FabricMultiHostDeployment/artifacts/channel/create-artifacts.sh
+++ FabricMultiHostDeployment/artifacts/channel/create-artifacts.sh
@@ -2,7 +2,7 @@
 chmod -R 0755 ./crypto-config
 # Delete existing artifacts
 rm -rf ./crypto-config
-rm genesis.block mychannel.tx
+rm genesis.block channel_1.tx
 rm -rf ../../channel-artifacts/*
 
 #Generate Crypto artifactes for organizations
@@ -13,8 +13,8 @@ rm -rf ../../channel-artifacts/*
 # System channel
 SYS_CHANNEL="sys-channel"
 
-# channel name defaults to "mychannel"
-CHANNEL_NAME="mychannel"
+# channel name defaults to "channel_1"
+CHANNEL_NAME="channel_1"
 
 echo $CHANNEL_NAME
 
@@ -23,13 +23,13 @@ configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outp
 
 
 # Generate channel configuration block
-configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./channel_1.tx -channelID $CHANNEL_NAME
 
-echo "#######    Generating anchor peer update for Org1MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+echo "#######    Generating anchor peer update for DiggipetMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./DiggipetMSPanchors.tx -channelID $CHANNEL_NAME -asOrg DiggipetMSP
 
-echo "#######    Generating anchor peer update for Org2MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+echo "#######    Generating anchor peer update for Vet_orgMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./VetorgMSPanchors.tx -channelID $CHANNEL_NAME -asOrg VetorgMSP
 
-echo "#######    Generating anchor peer update for Org3MSP  ##########"
-configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+echo "#######    Generating anchor peer update for Breeders_orgMSP  ##########"
+configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./BreedersorgMSPanchors.tx -channelID $CHANNEL_NAME -asOrg BreedersorgMSP
--- FabricMultiHostDeployment/setup1/vm4/create-certificate-with-ca/create-certificate-with-ca.sh
+++ FabricMultiHostDeployment/setup1/vm4/create-certificate-with-ca/create-certificate-with-ca.sh
@@ -24,22 +24,22 @@ createCretificateForOrderer() {
     OrganizationalUnitIdentifier: orderer' >${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml
 
   echo
-  echo "Register orderer"
+  echo "Register diggipet_orderer"
   echo
 
-  fabric-ca-client register --caname ca-orderer --id.name orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client register --caname ca-orderer --id.name diggipet_orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
   echo
-  echo "Register orderer2"
+  echo "Register vet_orderer"
   echo
 
-  fabric-ca-client register --caname ca-orderer --id.name orderer2 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client register --caname ca-orderer --id.name vet_orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
   echo
-  echo "Register orderer3"
+  echo "Register breeders_orderer"
   echo
 
-  fabric-ca-client register --caname ca-orderer --id.name orderer3 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client register --caname ca-orderer --id.name breeders_orderer --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
   echo
   echo "Register the orderer admin"
@@ -51,84 +51,84 @@ createCretificateForOrderer() {
   # mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/example.com
 
   # ---------------------------------------------------------------------------
-  #  Orderer
+  #  diggipet Orderer
 
-  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com
+  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com
 
   echo
-  echo "## Generate the orderer msp"
+  echo "## Generate the diggipet_orderer msp"
   echo
 
-  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp --csr.hosts orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client enroll -u https://diggipet_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/msp --csr.hosts orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
   cp ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/config.yaml
 
   echo
-  echo "## Generate the orderer-tls certificates"
+  echo "## Generate the diggipet_orderer-tls certificates"
   echo
 
-  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls --enrollment.profile tls --csr.hosts orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client enroll -u https://diggipet_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls --enrollment.profile tls --csr.hosts diggipet_orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/ca.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/server.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/server.key
 
-  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/msp/tlscacerts
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
   mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/diggipet_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
   # -----------------------------------------------------------------------
-  #  Orderer 2
+  #  vet_orderer
 
-  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com
-
-  echo
-  echo "## Generate the orderer2 msp"
-  echo
-
-  fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp --csr.hosts orderer2.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
-
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/config.yaml
+  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com
 
   echo
-  echo "## Generate the orderer2-tls certificates"
+  echo "## Generate the vet_orderer msp"
   echo
 
-  fabric-ca-client enroll -u https://orderer2:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls --enrollment.profile tls --csr.hosts orderer2.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client enroll -u https://vet_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/msp --csr.hosts vet_orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.key
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/msp/config.yaml
 
-  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+  echo
+  echo "## Generate the vet_orderer-tls certificates"
+  echo
+
+  fabric-ca-client enroll -u https://vet_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls --enrollment.profile tls --csr.hosts vet_orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/ca.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/server.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/server.key
+
+  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/msp/tlscacerts
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/vet_orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
   # ---------------------------------------------------------------------------
-  #  Orderer 3
-  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com
+  #  Breeders Orderer
+  mkdir -p ../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com
 
   echo
-  echo "## Generate the orderer3 msp"
+  echo "## Generate the breeders_orderer msp"
   echo
 
-  fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp --csr.hosts orderer3.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client enroll -u https://breeders_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/msp --csr.hosts breeders_orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/config.yaml
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/msp/config.yaml
 
   echo
-  echo "## Generate the orderer3-tls certificates"
+  echo "## Generate the breeders_orderer-tls certificates"
   echo
 
-  fabric-ca-client enroll -u https://orderer3:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls --enrollment.profile tls --csr.hosts orderer3.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
+  fabric-ca-client enroll -u https://breeders_orderer:ordererpw@localhost:9054 --caname ca-orderer -M ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls --enrollment.profile tls --csr.hosts breeders_orderer.example.com --csr.hosts localhost --tls.certfiles ${PWD}/fabric-ca/ordererOrg/tls-cert.pem
 
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/ca.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/server.crt
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/server.key
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/ca.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/signcerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/server.crt
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/keystore/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/server.key
 
-  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/tlscacerts
-  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+  mkdir ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/msp/tlscacerts
+  cp ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/tls/tlscacerts/* ${PWD}/../crypto-config/ordererOrganizations/example.com/orderers/breeders_orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
   # ---------------------------------------------------------------------------
 
   mkdir -p ../crypto-config/ordererOrganizations/example.com/users
